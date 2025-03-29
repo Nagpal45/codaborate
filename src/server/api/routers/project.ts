06f02b4960769da7e392428c5b612bdfcbd801c3
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2025-03-29 17:40:48 +0530

    uploadMeeting and getMeetings routes

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -4,28 +4,34 @@ import { pollCommits } from "@/lib/github";
 import { indexGithubRepo } from "@/lib/githubLoader";
 
 export const projectRouter = createTRPCRouter({
-  createProject: protectedProcedure.input(
-    z.object({
+  createProject: protectedProcedure
+    .input(
+      z.object({
         name: z.string(),
         githubUrl: z.string(),
         githubToken: z.string().optional(),
-    })
-  ).mutation(async ({ ctx, input }) => {
-    const project = await ctx.db.project.create({
-      data: {
-        githubUrl: input.githubUrl,
-        name: input.name,
-        userProjects: {
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const project = await ctx.db.project.create({
+        data: {
+          githubUrl: input.githubUrl,
+          name: input.name,
+          userProjects: {
             create: {
-                userId: ctx.user.userId!,
+              userId: ctx.user.userId!,
             },
+          },
         },
-        },
-    });
-    await indexGithubRepo(project.id, input.githubUrl, input.githubToken??'');
-    await pollCommits(project.id);
-    return project;
-  }),
+      });
+      await indexGithubRepo(
+        project.id,
+        input.githubUrl,
+        input.githubToken ?? "",
+      );
+      await pollCommits(project.id);
+      return project;
+    }),
   getProjects: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.project.findMany({
       where: {
@@ -38,48 +44,75 @@ export const projectRouter = createTRPCRouter({
       },
     });
   }),
-  getCommits: protectedProcedure.input(
-    z.object({
-      projectId: z.string(),
-    })
-  ).query(async ({ input, ctx }) => {
-    pollCommits(input.projectId).then().catch(console.error);
-    return await ctx.db.commit.findMany({
+  getCommits: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      pollCommits(input.projectId).then().catch(console.error);
+      return await ctx.db.commit.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+      });
+    }),
+  saveAnswer: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+        question: z.string(),
+        answer: z.string(),
+        filesReferences: z.any(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.db.question.create({
+        data: {
+          answer: input.answer,
+          projectId: input.projectId,
+          question: input.question,
+          filesReferences: input.filesReferences,
+          userId: ctx.user.userId!,
+        },
+      });
+    }),
+  getQuestions: protectedProcedure
+    .input(z.object({ projectId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      return await ctx.db.question.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }),
+    uploadMeeting: protectedProcedure.input(z.object({projectId: z.string(), meetingAudioURL : z.string(), name: z.string()})).mutation(async ({input, ctx}) => {
+      const meeting = await ctx.db.meeting.create({
+        data: {
+          projectId: input.projectId,
+          meetingAudioURL: input.meetingAudioURL,
+          name: input.name,
+          status: "PROCESSING",
+        },
+      })
+      return meeting;
+    }),
+  getMeetings: protectedProcedure.input(z.object({projectId: z.string()})).query(async ({input, ctx}) => {
+    return await ctx.db.meeting.findMany({
       where: {
         projectId: input.projectId,
-      },
-    });
-  }),
-  saveAnswer: protectedProcedure.input(
-    z.object({
-      projectId: z.string(),
-      question: z.string(),
-      answer: z.string(),
-      filesReferences: z.any()
-    })
-  ).mutation(async ({ input, ctx }) => {
-    return await ctx.db.question.create({
-      data: {
-        answer: input.answer,
-        projectId: input.projectId,
-        question: input.question,
-        filesReferences: input.filesReferences,
-        userId: ctx.user.userId!,
-      },
-    });
-  }),
-  getQuestions: protectedProcedure.input(z.object({projectId: z.string()})).query(async ({input, ctx}) => {
-    return await ctx.db.question.findMany({
-      where: {
-        projectId: input.projectId,
-      },
-      include: {
-        user: true,
       },
       orderBy: {
         createdAt: "desc",
-      }, 
-    });
+      },
+    })
   }
   ),
 });
