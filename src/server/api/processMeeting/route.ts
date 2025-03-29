e10326f2c16e34cb95f7d3a406d3cf649faafd24
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2025-03-30 01:11:59 +0530

    processMeeting route

---
+++ src/server/api/processMeeting/route.ts
@@ -0,0 +1,51 @@
+import { processMeeting } from "@/lib/assemblyai";
+import { db } from "@/server/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const bodyParser = z.object({
+    meetingAudioURL: z.string(),
+    projectId: z.string(),
+    meetingId: z.string(),
+})
+
+export const maxDuration = 300;
+
+export const POST = async (req: NextRequest) => {
+    const {userId} = await auth();
+    if(!userId) {
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+    }
+
+    try{
+        const body = await req.json();
+        const {meetingAudioURL, projectId, meetingId} = bodyParser.parse(body);
+        const summaries = await processMeeting(meetingAudioURL);
+        await db.issue.createMany({
+            data: summaries.map((summary) => ({
+                start: summary.start,
+                end: summary.end,
+                gist: summary.gist,
+                headline: summary.headline,
+                summary: summary.summary,
+                projectId,
+                meetingId,
+            })),
+        });
+        await db.meeting.update({
+            where: {
+                id: meetingId,
+            },
+            data: {
+                status: "COMPLETED",
+                name: summaries[0]!.headline,
+            },
+        });
+        return NextResponse.json({message: "Meeting processed successfully"}, {status: 200});
+    }catch(error) {
+        console.error(error);
+        return NextResponse.json({error: "Internal Server Error"}, {status: 500});
+    }
+};
+
