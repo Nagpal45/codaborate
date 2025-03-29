90e98bfcc1119fc3f523dfb34c3b4ee9f9062cfa
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2025-03-30 00:55:47 +0530

    transcribe meetingURL using assembly

--- src/lib/assemblyai.ts
+++ src/lib/assemblyai.ts
@@ -1,10 +1,34 @@
 import {AssemblyAI} from 'assemblyai';
 
-const client = new AssemblyAI({apiKey: process.env.ASSEMBLYAI_API_KEY!});
+const client = new AssemblyAI({
+    apiKey: process.env.ASSEMBLYAI_API_KEY!,
+});
 
 export const msToTime = (duration: number) => {
     const seconds = duration / 1000;
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = Math.floor(seconds % 60);
     return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
+
+export const processMeeting = async (meetingAudioURL: string) => {
+    const transcript = await client.transcripts.transcribe({
+        audio: meetingAudioURL,
+        auto_chapters: true,
+    })
+    const summaries = transcript.chapters?.map(chapter => ({
+        start: msToTime(chapter.start),
+        end: msToTime(chapter.end),
+        gist: chapter.gist,
+        headline : chapter.headline,
+        summary: chapter.summary,
+    })) || [];
+
+    if(!transcript.text) {
+        throw new Error('No transcript text found');
+    }
+
+    return {
+        transcript, summaries,
+    }
 }
