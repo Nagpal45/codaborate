39c1647b41af58ee2d65cd55112083c17be7b0ba
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2025-04-02 22:27:06 +0530

    check credits of user and filecount on backend and decrement

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -13,6 +13,16 @@ export const projectRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
+      const user = await ctx.db.user.findUnique({ where: { id: ctx.user.userId! }, select: { credits: true } });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      const currentCredits = user.credits || 0;
+      const fileCount = await checkCredits(input.githubUrl, input.githubToken);
+
+      if(currentCredits < fileCount) {
+        throw new Error("Not enough credits");
+      }
       const project = await ctx.db.project.create({
         data: {
           githubUrl: input.githubUrl,
@@ -30,6 +40,10 @@ export const projectRouter = createTRPCRouter({
         input.githubToken ?? "",
       );
       await pollCommits(project.id);
+      await ctx.db.user.update({
+        where: { id: ctx.user.userId! },
+        data: { credits: { decrement: fileCount } },
+      });
       return project;
     }),
   getProjects: protectedProcedure.query(async ({ ctx }) => {
